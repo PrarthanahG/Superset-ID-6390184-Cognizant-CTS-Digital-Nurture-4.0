b13c8678d98ffdeda73d28ad7c3631aaa665f634
PrarthanahG <prarthanahg2004@gmail.com>
2025-07-06 19:44:13 +0530

    updated CTS Digital Nurture 4.0

---
+++ Spring Core Maven/Configuring a Basic Spring Application/Code/Library Management/src/main/java/com/library/service/bookservice.java
@@ -0,0 +1,10 @@
+package com.library.service;
+
+import com.library.repository.BookRepository;
+
+public class BookService {
+
+public void displayBooks() {
+	        System.out.print("Book Name");
+	    }
+}
